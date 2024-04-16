fcecc8c16492527182896f6a2f286ebea21084c9
rafez963 <rafaelmeza963san@gmail.com>
2024-04-16 16:15:11 -0500

    Challenge one completed.

---
+++ seccionUno/fileOne.js
@@ -0,0 +1,31 @@
+// DEfinir variables 
+
+// destino del viaje 
+const destinoViaje =  pront("Por favor ingresa el destino de tu viaje: ")
+console.log('El destino del viaje es : ',destinoViaje)
+
+// cantidad de dias
+let cantidadDiasViaje = pront("Por favor ingrese la cantidad de dias para su viaje: ")
+console.log(`La cantidiad de dias para su viaje es: ${cantidadDiasViaje}`)
+
+//presupuesto del viaje 
+let presupuestoViaje = pront("Por favor ingrese el presupuesto para su viaje: ")
+console.log()
+
+// estimado minimo para hacer el viaje
+const presupuestoMinimo = 50000 
+
+//presupuesto estimado por dias
+let presupuestoDiaEstimado = presupuesto / cantidadDiasViaje
+
+// costo total estimado para el viaje
+const costoTotalEstimado = cantidadDiasViaje * presupuestoDiaEstimado
+
+// condiciones para el viaje 
+
+if (presupuestoMinimo >= costoTotalEstimado ){
+    console.log("Tu presupuesto actual es suficiente para cubrir el viaje")
+}
+else{
+    console.log("tu presupuesto actual no es suficiente para cubir el viaje")
+}
