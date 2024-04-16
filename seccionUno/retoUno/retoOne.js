709d6472fb58e4affa28b40e07e11c6a1769e538
rafez963 <rafaelmeza963san@gmail.com>
2024-04-16 17:13:03 -0500

    files retoUno

--- seccionUno/retoUno/retoOne.js
+++ seccionUno/retoUno/retoOne.js
@@ -1,22 +1,22 @@
 // DEfinir variables 
 
 // destino del viaje 
-const destinoViaje =  pront("Por favor ingresa el destino de tu viaje: ")
+const destinoViaje =  prompt("Por favor ingresa el destino de tu viaje: ")
 console.log('El destino del viaje es : ',destinoViaje)
 
 // cantidad de dias
-let cantidadDiasViaje = pront("Por favor ingrese la cantidad de dias para su viaje: ")
+let cantidadDiasViaje = prompt("Por favor ingrese la cantidad de dias para su viaje: ")
 console.log(`La cantidiad de dias para su viaje es: ${cantidadDiasViaje}`)
 
 //presupuesto del viaje 
-let presupuestoViaje = pront("Por favor ingrese el presupuesto para su viaje: ")
-console.log()
+let presupuestoViaje = prompt("Por favor ingrese el presupuesto para su viaje: ")
+console.log(`su presupuesto es ${presupuestoViaje }`)
 
 // estimado minimo para hacer el viaje
 const presupuestoMinimo = 50000 
 
 //presupuesto estimado por dias
-let presupuestoDiaEstimado = presupuesto / cantidadDiasViaje
+let presupuestoDiaEstimado = presupuestoMinimo / cantidadDiasViaje
 
 // costo total estimado para el viaje
 const costoTotalEstimado = cantidadDiasViaje * presupuestoDiaEstimado
