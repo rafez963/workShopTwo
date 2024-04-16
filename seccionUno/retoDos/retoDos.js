5d9155557c3664c5c6b0ba71de4c6e8b61dab414
rafez963 <rafaelmeza963san@gmail.com>
2024-04-16 17:45:00 -0500

    update files retoDos

---
+++ seccionUno/retoDos/retoDos.js
@@ -0,0 +1,11 @@
+//Datos principales 
+//nombre del souvenir
+let nombreSouvenir = prompt("Ingrese el nombre del souvenir")
+console.log(`El nombre del souvenir es: ${nombreSouvenir}`)
+//costo del souvenir
+let costoSouvenir = parseFloat(prompt("Ingrese el costo del souvenir"))
+console.log(`El nombre del souvenir es: ${costoSouvenir}`)
+//Disponibilidad del sourvenir
+let disponibilidadSouvenir = prompt(`¿El souvenir esta disponible? (si/no)`)
+//Almacenamiento de la disponibilidad 
+let estadoDisponible 
